1728e6262f4f97496e72a4fe41ffbd042bdc4862
Pavlakiss <pavlosgeorgiou1@gmail.com>
2024-01-18 22:06:18 +0100

    Made Public Variable For Max Players

--- Y2B2 Project/Assets/Pavlos Scripts/RoomManager.cs
+++ Y2B2 Project/Assets/Pavlos Scripts/RoomManager.cs
@@ -12,6 +12,7 @@ public class RoomManager : MonoBehaviourPunCallbacks
     public TMP_Text playerListText; // Text element to display the list of players in the room
     public Button joinButton; // Button to join a room
     public Button createButton; // Button to create a room
+    public byte maxPlayers = 2; // Max players allowed in the room
 
     void Start()
     {
@@ -27,12 +28,12 @@ public class RoomManager : MonoBehaviourPunCallbacks
         if (!string.IsNullOrEmpty(roomName))
         {
             RoomOptions options = new RoomOptions();
-            options.MaxPlayers = 2; // You can set the max players for the room
+            options.MaxPlayers = maxPlayers; // You can set the max players for the room
             PhotonNetwork.CreateRoom(roomName, options);
         }
         else
         {
-            Debug.LogError("Room name is empty or null."); // Add this line
+            Debug.LogError("Room name is empty or null."); 
         }
     }
 
@@ -49,7 +50,6 @@ public class RoomManager : MonoBehaviourPunCallbacks
     {
         roomInfoText.text = "Joined Room: " + PhotonNetwork.CurrentRoom.Name;
         UpdatePlayerList();
-        PhotonNetwork.LoadLevel("Game"); // Load the game scene
     }
 
     public override void OnCreateRoomFailed(short returnCode, string message)
@@ -80,6 +80,12 @@ public class RoomManager : MonoBehaviourPunCallbacks
         foreach (var player in PhotonNetwork.PlayerList)
         {
             playerListText.text += player.NickName + "\n";
+        }
+        // Check if the number of players in the room has reached the maxPlayers
+        if (PhotonNetwork.PlayerList.Length == maxPlayers)
+        {
+            // Load the game scene
+            PhotonNetwork.LoadLevel("Game_Ai");
         }
     }
 }
