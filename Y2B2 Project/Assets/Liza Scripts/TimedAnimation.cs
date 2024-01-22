97fddaff7b9b26f3135e647f7bcd38ab7b1603ed
lizathebubu <127210962+lizathebubu@users.noreply.github.com>
2024-01-22 15:50:04 +0100

    almost finished the game write scene

--- Y2B2 Project/Assets/Liza Scripts/TimedAnimation.cs
+++ Y2B2 Project/Assets/Liza Scripts/TimedAnimation.cs
@@ -10,12 +10,16 @@ public class TimedAnimation : MonoBehaviour
 
     public Animator textAnim;
     public Animator imageAnim;
+    public Animator headlineInputAnim;
+    public Animator timerAnim;
 
     IEnumerator PlayAnimation()
     {
         yield return new WaitForSeconds(animationTime);
         textAnim.SetTrigger("Play");
         imageAnim.SetTrigger("Play");
+        headlineInputAnim.SetTrigger("Play");
+        timerAnim.SetTrigger("Play");
     }
 
     // Start is called before the first frame update
