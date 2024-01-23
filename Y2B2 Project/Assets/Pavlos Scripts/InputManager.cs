7a81729e5f26cb57d483996228e785b9778c20a4
Pavlakiss <pavlosgeorgiou1@gmail.com>
2024-01-23 19:52:16 +0100

    Update InputManager.cs

--- Y2B2 Project/Assets/Pavlos Scripts/InputManager.cs
+++ Y2B2 Project/Assets/Pavlos Scripts/InputManager.cs
@@ -1,18 +1,18 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
 public class InputManager : MonoBehaviour
 {
+    public static List<string> playerResponses = new List<string>(); // This list will store the responses
     public TMP_InputField inputField;
 
     public void SubmitResponse()
     {
         if (!string.IsNullOrEmpty(inputField.text))
         {
-           
-            inputField.text = ""; // Clear the field after submission
+            playerResponses.Add(inputField.text); // Add the input text to the responses list
+            inputField.text = ""; // Clear the input field
         }
     }
 }
