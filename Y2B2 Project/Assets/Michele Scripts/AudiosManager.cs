0e1428360f987f4a2590b8c51abb08d47ae4ed0d
ItsTosti <hazenbergthorsten@gmail.com>
2024-01-23 20:45:18 +0100

    added all the scenes

--- Y2B2 Project/Assets/Michele Scripts/AudiosManager.cs
+++ Y2B2 Project/Assets/Michele Scripts/AudiosManager.cs
@@ -9,14 +9,14 @@ public class AudiosManager : MonoBehaviour
 
     void Awake()
     {
-     foreach (Sound s in sounds)
-     {
-        s.source = gameObject.AddComponent<AudioSource>();
-        s.source.clip = s.clip;
-        s.source.volume = s.volume;
-        s.source.pitch = s.pitch;
-        s.source.loop = s.loop;
-     }  
+        foreach (Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+        }  
     }
 
     void Start ()
